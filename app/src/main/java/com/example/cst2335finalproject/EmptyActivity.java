b09cea02ec310d4bea0e7d294cd0906b5e964e2c
unknown <lindsay.c.fumerton@gmail.com>
2024-04-11 10:59:25 -0400

    added navigation drawer to details page

--- app/src/main/java/com/example/cst2335finalproject/EmptyActivity.java
+++ app/src/main/java/com/example/cst2335finalproject/EmptyActivity.java
@@ -1,15 +1,38 @@
 package com.example.cst2335finalproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class EmptyActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class EmptyActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empty);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, myToolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
         Bundle dataToPass = getIntent().getExtras();
 
         DetailsFragment dFragment = new DetailsFragment();
@@ -18,5 +41,45 @@ public class EmptyActivity extends AppCompatActivity {
                 .beginTransaction()
                 .replace(R.id.fragmentLocation, dFragment)
                 .commit();
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.home) {
+            Intent home = new Intent(this, MainActivity.class);
+            startActivity(home);
+        }
+        else if (id == R.id.favourites) {
+            Intent favourites = new Intent(this, Favourites.class);
+            startActivity(favourites);
+        }
+        else if (id == R.id.exit) {
+            finishAffinity();
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return false;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        String message = null;
+        int id = item.getItemId();
+        if (id == R.id.help) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("Help")
+                    .setMessage("Enter a search term into the search bar to find articles!")
+                    .create().show();
+            message = "You clicked the help button!";
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        return true;
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
