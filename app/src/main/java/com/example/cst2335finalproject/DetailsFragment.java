82d5f8ed30f2c2f5accfeaf794293e9b4eeed6da
Jacynthe <jacynthec2@gmail.com>
2024-04-11 19:19:10 -0400

    adding javadocs

--- app/src/main/java/com/example/cst2335finalproject/DetailsFragment.java
+++ app/src/main/java/com/example/cst2335finalproject/DetailsFragment.java
@@ -28,10 +28,25 @@ public class DetailsFragment extends Fragment {
 
     ArrayList<String> favourites = new ArrayList<>();
 
+    /**
+     * None
+     */
     public DetailsFragment() {
         // Required empty public constructor
     }
 
+    /**
+     *
+     * @param inflater The LayoutInflater object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-null, this is the parent view that the fragment's
+     * UI should be attached to.  The fragment should not add the view itself,
+     * but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     *
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -94,6 +109,11 @@ public class DetailsFragment extends Fragment {
         return result;
 
     }
+
+    /**
+     *
+     * @param context
+     */
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -101,6 +121,9 @@ public class DetailsFragment extends Fragment {
         parentActivity = (AppCompatActivity) context;
     }
 
+    /**
+     *
+     */
     private class MyListAdapter extends BaseAdapter {
         @Override
         public int getCount() {
